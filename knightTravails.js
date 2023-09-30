32afe44a7c57809e61c32dc339484603fd9d46fb
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-09-30 16:36:41 +0500

    Setup general code structure

--- knightTravails.js
+++ knightTravails.js
@@ -1,7 +1,34 @@
-class Node {}
+class Node {
+  constructor(coords, parentNode) {
+    this.x = coords.x;
+    this.y = coords.y;
+    this.parentNode = parentNode;
+    this.children = [];
+  }
+}
 
-class Tree {}
+class Tree {
+  constructor(startCoords, endCoords) {
+    this.root = this.#setupTree(startCoords, endCoords);
+  }
+
+  #setupTree(startCoords, endCoords) {}
+
+  #LDRSearch(coords) {}
+
+  getPathToNode(coords) {
+    const node = this.#LDRSearch(coords);
+  }
+}
+
+function prettyPrint(nodesArray) {}
 
 module.exports = function knightTravails(startCoords, endCoords) {
-  return "Hello world";
+  const tree = new Tree(startCoords, endCoords);
+  // build a tree
+  // build nodes on the tree until you get the node with endCoords
+  // get all the nodes from that node up to a parent node
+  const pathToNeededTile = tree.getPathToNode(endCoords);
+  // print the results in a pretty fashion
+  prettyPrint(nodesArray);
 };
